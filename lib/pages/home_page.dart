ada9900a80214c83db4b593753c94ac102bb0edd
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-18 15:29:08 -0300

    SearchPage widget - Form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_cubit/cubits/cubits.dart';
+import 'package:weather_cubit/pages/pages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final weatherCubit = context.read<WeatherCubit>();
+
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const SearchPage();
+                }),
+              );
+              if (_city != null) {
+                weatherCubit.fetchWeather(_city!);
+              }
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
