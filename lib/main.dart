dfe5ba2cccef0927e434765de0e02b2fde34ad46
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-18 14:52:50 -0300

    Weather Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_cubit/cubits/cubits.dart';
 import 'package:weather_cubit/pages/pages.dart';
+import 'package:weather_cubit/repositories/repositories.dart';
+import 'package:weather_cubit/services/services.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
@@ -10,9 +17,26 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: HomePage(),
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+        weatherApiServices: WeatherApiServices(httpClient: http.Client()),
+      ),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<WeatherCubit>(
+            create: (context) => WeatherCubit(
+              weatherRepository: context.read<WeatherRepository>(),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Weather App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true),
+          home: const HomePage(),
+        ),
       ),
     );
   }
