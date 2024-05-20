38107133bd1e449ce33fcadc28432d3baa7dae25
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-20 20:20:13 -0300

    refactor theme app state

--- lib/main.dart
+++ lib/main.dart
@@ -32,22 +32,25 @@ class MainApp extends StatelessWidget {
             create: (context) => TempSettingsCubit(),
           ),
           BlocProvider<ThemeCubit>(
-            create: (context) => ThemeCubit(
-              weatherCubit: context.read<WeatherCubit>(),
-            ),
+            create: (context) => ThemeCubit(),
           ),
         ],
-        child: BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) {
-            return MaterialApp(
-              title: 'Weather App',
-              debugShowCheckedModeBanner: false,
-              theme:state.appTheme == AppTheme.light
-                  ? ThemeData.light(useMaterial3: true)
-                  : ThemeData.dark(useMaterial3: true),
-              home: const HomePage(),
-            );
+        child: BlocListener<WeatherCubit, WeatherState>(
+          listener: (context, state) {
+            context.read<ThemeCubit>().setTheme(state.weather.temp);
           },
+          child: BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return MaterialApp(
+                title: 'Weather App',
+                debugShowCheckedModeBanner: false,
+                theme: state.appTheme == AppTheme.light
+                    ? ThemeData.light(useMaterial3: true)
+                    : ThemeData.dark(useMaterial3: true),
+                home: const HomePage(),
+              );
+            },
+          ),
         ),
       ),
     );
